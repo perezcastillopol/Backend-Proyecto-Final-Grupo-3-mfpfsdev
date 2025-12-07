cd6fb8e5d008ba722672d62854b2b508e387a454
fkaexe <josemiguelmartinezay@gmail.com>
2025-12-07 23:58:42 +0100

    fix: env file

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -3,13 +3,13 @@ import { Router } from 'express';
 import apiUser from "./api/user.routes.js";
 import apiTrips from "./api/trip.routes.js";
 import apiModality from "./api/modality.routes.js";
-import tripInvitationRoutes from "./routes/trip-invitation.routes.js";
+import tripInvitationRoutes from "./api/trip-invitation.routes.js";
 
 const router = Router();
 
 router.use('/users', apiUser);
 router.use('/trips', apiTrips);
 router.use('/modality', apiModality);
-app.use('/api', tripInvitationRoutes);
+router.use('/api', tripInvitationRoutes);
 
 export default router;
