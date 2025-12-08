6a7baf6be9b4d87533b1d5934e0737f91ede722e
cblanco <cris.blanco93@gmail.com>
2025-12-08 12:12:09 +0100

    Gestion de las modalidades preferidas de usuario y captura de excepciones de creacion de usuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,9 @@ import {
   selectUserById,
   update,
   selectUserByEmail,
+  insertModalityUser,
+  deleteModalityUser,
+  getModalitiesByUser
 } from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
@@ -16,6 +19,10 @@ export const getAllUsers = async (req, res) => {
 };
 
 export const getUserById = async (req, res) => {
+  const { userId } = req.params;
+  const mods = await getModalitiesByUser(userId);
+  req.user.interests = mods;
+  console.log('esto recupero: ',mods)
   // checkIdUser ya mete el usuario en req.user
   res.json(req.user);
 };
@@ -23,17 +30,46 @@ export const getUserById = async (req, res) => {
 /***********************************************************INSERT*************************************/
 
 export const createUser = async (req, res) => {
-  const { insertId } = await insertUser(req.body);
-  const result = await selectUserById(insertId);
-  res.json(result);
-};
+  try {
+    const { insertId } = await insertUser(req.body);
+    const { interests } = req.body;
+    if (Array.isArray(interests)) {
+      for (const i of interests) {
+        const { id } = i;
+        await insertModalityUser({ users_id: insertId, id });
+      }
+    }
+    const result = await selectUserById(insertId);
+    return res.json(result);
 
+  } catch (error) {
+    if (error.status) {
+      return res.status(error.status).json({ message: error.message });
+    }
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.status(400).json({
+        message: "El email ya existe en nuestra base de datos"
+      });
+    }
+    console.error(error);
+    return res.status(500).json({
+      message: "Error en el servidor"
+    });
+  }
+};
 /***********************************************************UPDATE****************************************/
 
 export const updateUser = async (req, res) => {
-  console.log('entro al controller');
   const { userId } = req.params;
+  await deleteModalityUser(userId);
   await update(userId, req.body);
+  const {interests} = req.body;
+  if (Array.isArray(interests)) {
+      for (const i of interests) {
+        const {id} = i;
+        await insertModalityUser({users_id: userId, id});
+      }
+  } 
   const result = await selectUserById(userId);
   res.json(result);
 };
